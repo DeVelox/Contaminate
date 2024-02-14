5d16f95490da8588b5adc2f77c9e9863b0ade8ab
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-02-14 19:14:05 +0100

    Lag?

--- EnemySpawner.gd
+++ EnemySpawner.gd
@@ -5,7 +5,6 @@ extends Node
 
 var timer_running: float
 var enemies: Array[StaticBody2D]
-var stop_moving: bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	timer_running = timer
@@ -17,15 +16,15 @@ func _process(delta: float) -> void:
 		timer_running -= delta
 	else:
 		var enemy_instance := enemy.instantiate()
-		var new_position := player.position + Vector2.ONE.normalized().rotated(randf_range(0, 2*PI)) * randf_range(190, 300)
+		var new_position := player.position + Vector2.ONE.normalized().rotated(randf_range(0, 2*PI)) * randf_range(270, 400)
 		
 		enemy_instance.position = new_position
 		add_child(enemy_instance)
 		timer_running = timer
 		enemies.append(enemy_instance)
 	for i in enemies:
-		if i.position.distance_squared_to(player.position) < pow(190, 2):
-			stop_moving = true
-		else:
-			i.position = lerp(i.position, player.position, 0.1 * delta)
+		if i.position.distance_squared_to(player.position) < pow(270, 2):
+			i.chase = true
+		if i.chase:
+			i.position = lerp(i.position, player.position, 0.2 * delta)
 		
