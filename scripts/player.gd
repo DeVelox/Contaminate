157e3f5cd218461f5ce90751c90d9aaf135e3bc5
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-02-17 18:06:23 +0100

    Camera options

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,9 @@
 class_name Player extends CharacterBody2D
 
 const SPEED = 300.0
+const CAMERA_SPEED = 50.0
 
+@export_enum("light_ahead", "camera_ahead", "camera_drag") var camera: String
 
 func _physics_process(delta: float) -> void:
 	var direction := (
@@ -11,6 +13,18 @@ func _physics_process(delta: float) -> void:
 		velocity = lerp(velocity, direction * SPEED, 0.25)
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, 0.25)
+		
+	match camera:
+		"light_ahead":
+			$PointLight2D.position = lerp($PointLight2D.position, direction * CAMERA_SPEED, 0.1)
+			$PointLight2D2.position = lerp($PointLight2D2.position, direction * CAMERA_SPEED, 0.1)
+			$EnemyCulling.position = lerp($EnemyCulling.position, direction * CAMERA_SPEED, 0.1)
+		"camera_ahead":
+			$Camera2D.offset = lerp($Camera2D.offset, direction * CAMERA_SPEED, 0.1)
+		"camera_drag":
+			$Camera2D.offset = lerp($Camera2D.offset, -direction * CAMERA_SPEED, 0.1)
+		_:
+			pass
 
 	move_and_slide()
 
