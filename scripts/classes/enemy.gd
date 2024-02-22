690f50440af372bb855da667b5d77b325fb8d973
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-02-22 15:56:43 +0100

    Speed modifier implementation

--- scripts/classes/enemy.gd
+++ scripts/classes/enemy.gd
@@ -1,5 +1,11 @@
 class_name Enemy extends StaticBody2D
 
+enum SpeedGroup {SHOCK, KNEE_CAP, BOOST, MISC}
+
+const BASE_SPEED = 100.0
+const MIN_SPEED = 10.0
+const MAX_SPEED = 200.0
+
 @export var health := 100
 @export var attack := 1
 @export var speed := 100.0
@@ -11,10 +17,30 @@ var velocity := Vector2.ZERO
 var physics := true
 var infection_count: int = 0
 
+var speed_flat: Array[float]
+var speed_multi: Array[float]
+
+var enemy_id: int
+static var id: int
+
 @onready var infection_timer: Timer = $InfectionTimer
+
+func _ready() -> void:
+	id += 1
+	enemy_id = id
+	
+	speed_flat.resize(SpeedGroup.size())
+	speed_multi.resize(SpeedGroup.size())
+	
+	apply_speed_mod(5, -50, true, SpeedGroup.KNEE_CAP)
+	apply_speed_mod(5, -20, true, SpeedGroup.KNEE_CAP)
+	apply_speed_mod(2, 30, false, SpeedGroup.BOOST)
 
 
 func _physics_process(delta: float) -> void:
+	speed = clamp((BASE_SPEED + speed_flat.reduce(_sum)) * (1 + speed_multi.reduce(_sum)), MIN_SPEED, MAX_SPEED)
+	if enemy_id == 1:
+		print_debug(speed)
 	motion = (direction * speed * delta)
 	velocity = lerp(velocity, motion, 0.1)
 	move_and_collide(velocity, false, 1.0)
@@ -42,6 +68,26 @@ func kill() -> void:
 	get_parent().enemy_instances.erase(self)
 	queue_free()
 
+func apply_speed_mod(duration: float, amount: float, multi: bool = false, group: SpeedGroup = SpeedGroup.MISC) -> void:
+	if group == SpeedGroup.MISC:
+		if multi:
+			speed_multi[group] += amount / 100.0
+			await get_tree().create_timer(duration).timeout
+			speed_multi[group] -= amount / 100.0
+		else:
+			speed_flat[group] += amount
+			await get_tree().create_timer(duration).timeout
+			speed_flat[group] -= amount
+	else:
+		if multi and absf(amount / 100.0) > absf(speed_multi[group]):
+			speed_multi[group] = amount / 100.0
+			await get_tree().create_timer(duration).timeout
+			speed_multi[group] = 0.0
+		elif absf(amount) > absf(speed_flat[group]):
+			speed_flat[group] = amount
+			await get_tree().create_timer(duration).timeout
+			speed_flat[group] = 0.0
+
 func infect() -> void:
 	if infection_timer.is_stopped():
 		infection_timer.start()
@@ -60,3 +106,6 @@ func _check_infection() -> void:
 		try_kill()
 	else:
 		infection_timer.stop()
+
+func _sum(a: float, b: float) -> float:
+	return a + b
