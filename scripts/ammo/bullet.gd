10de98105b6e8eef3ed6c89ba8dbd3a91f3fe868
RonShleif <rr.ron.rr@gmail.com>
2024-02-27 01:15:02 +0200

    upgrades and signals

--- scripts/ammo/bullet.gd
+++ scripts/ammo/bullet.gd
@@ -7,8 +7,9 @@ func _on_body_entered(body: Node2D) -> void:
 			if is_crit:
 				UpgradeManager.on_crit.emit(body)
 				damage *= 2
+				bonus_damage *= 2
 			UpgradeManager.on_hit.emit(body)
-			body.damage(damage)
+			body.damage(damage + bonus_damage)
 			penetration -= 1
 		else:
 			queue_free()
