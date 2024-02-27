6be39cf7a1ce7ba6e303d052ceaaef8212b38678
RonShleif <rr.ron.rr@gmail.com>
2024-02-27 23:50:18 +0200

    crit mastery change

--- scripts/upgrades/crit_mastery/crit_mastery_manager.gd
+++ scripts/upgrades/crit_mastery/crit_mastery_manager.gd
@@ -1,7 +1,8 @@
 extends Upgrade
 
 var crit_chance_bonus: float = 0.05
-var buff_duration: float = 3.0
+var buff_chance: float = 0.25
+var buff_duration: float = 5.0
 var buff_type = MechanicsManager.BuffType.CRIT_CHANCE
 var buff_bucket = MechanicsManager.BuffBucket.MISC
 var multi = false
@@ -12,14 +13,14 @@ func _init() -> void:
 
 
 func add() -> void:
-	if not UpgradeManager.on_crit.is_connected(_add_crit):
-		UpgradeManager.on_crit.connect(_add_crit)
+	if not UpgradeManager.on_hit.is_connected(_add_crit):
+		UpgradeManager.on_hit.connect(_add_crit)
 	UpgradeManager.available_upgrades.erase(uname)
 
 
 func remove() -> void:
-	if UpgradeManager.on_crit.is_connected(_add_crit):
-		UpgradeManager.on_crit.disconnect(_add_crit)
+	if UpgradeManager.on_hit.is_connected(_add_crit):
+		UpgradeManager.on_hit.disconnect(_add_crit)
 	UpgradeManager.available_upgrades.append(uname)
 
 
@@ -27,7 +28,7 @@ func _register() -> void:
 	uname = "Crit Mastery"
 	type = Type.GENERAL
 	rarity = Rarity.COMMON
-	description = "Whenever you crit increase your crit chance for a short duration."
+	description = "Whenever you hit increase your crit chance for a short duration."
 	logic = "res://scripts/upgrades/crit_mastery/crit_mastery_manager.gd"
 	icon = "res://assets/art/upgrades/william-tell-skull.svg"
 	level = 0
@@ -36,6 +37,8 @@ func _register() -> void:
 
 
 func _add_crit(_enemy: Enemy) -> void:
+	if randf() > buff_chance:
+		return
 	var player = UpgradeManager.get_player()
 	MechanicsManager.apply_buff(
 		player, buff_duration, crit_chance_bonus, buff_type, multi, buff_bucket
